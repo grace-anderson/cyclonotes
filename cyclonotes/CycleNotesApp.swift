65f8cee597e7469a7a0d5da2d9fa55cfa060cc0b
HelenA <helen.g.anderson@me.com>
2025-11-02 21:24:59 +1100

    Update telemetryDeck

--- cyclonotes/CycleNotesApp.swift
+++ cyclonotes/CycleNotesApp.swift
@@ -16,12 +16,14 @@ struct CycleNotesApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     init() {
-            let config = TelemetryDeck.Config(appID: "3C3CFCEF-E386-4489-8ED0-E043B18C7E4D")
-            TelemetryDeck.initialize(config: config)
-        }
+        let config = TelemetryDeck.Config(appID: "3C3CFCEF-E386-4489-8ED0-E043B18C7E4D")
+        TelemetryDeck.initialize(config: config)
+    }
     
     var body: some Scene {
-        WindowGroup { RootView() }
-            .modelContainer(for: [Ride.self, RoutePoint.self, RideNote.self, RidePhoto.self])
+        WindowGroup {
+            RootView()
+        }
+        .modelContainer(for: [Ride.self, RoutePoint.self, RideNote.self, RidePhoto.self])
     }
 }
